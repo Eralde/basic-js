d71d1b455575d640de29b07ec152e8a66053465f
Eralde <eralde.g@gmail.com>
2023-04-16 21:52:03 +0200

    task: implement 'transform' function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create transformed array based on the control sequences that original
  * array contains
@@ -13,9 +11,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const doesAffectPrev = (cmd) => ['--discard-prev', '--double-prev'].includes(cmd);
+const doesAffectNext = (cmd) => ['--discard-next', '--double-next'].includes(cmd);
+
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+
+  const result = [];
+
+  for (let i = 0, len = arr.length; i < len; ++i) {
+    const item = arr[i];
+    const type = typeof item;
+
+    if (
+      type === 'number'
+      || type !== 'string'
+    ) {
+      result.push(item);
+      continue;
+    }
+
+    switch (item) {
+      case '--discard-next':
+        i++;
+
+        if (doesAffectPrev(arr[i + 1])) {
+          i++;
+        }
+        continue;
+
+      case '--discard-prev':
+        result.pop();
+
+        if (doesAffectNext(arr[i - 1])) {
+          result.pop();
+        }
+        continue;
+
+      case '--double-next':
+        if (typeof arr[i + 1] === 'number') {
+          result.push(arr[i + 1]);
+        }
+        continue;
+
+      case '--double-prev':
+        if (typeof arr[i - 1] === 'number') {
+          result.push(result[result.length - 1]);
+        }
+        continue;
+    }
+
+    result.push(item);
+  }
+
+  return result;
 }
 
 module.exports = {
