231cec7d4bf39c26b1dba6222b28f53931af6711
Eralde <eralde.g@gmail.com>
2023-04-16 12:51:01 +0200

    task: implement 'dateSample' function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,25 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+
+  const activityNum = Number(sampleActivity);
+
+  if (
+    isNaN(activityNum)
+    || !isFinite(activityNum)
+    || activityNum <= 0
+    || activityNum > MODERN_ACTIVITY
+  ) {
+    return false;
+  }
+
+  const k = 0.693 / HALF_LIFE_PERIOD;
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / activityNum) / k);
 }
 
 module.exports = {
