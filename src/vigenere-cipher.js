65f5e1bb449b49d9ce978cdd191bb868b5e65fa1
Eralde <eralde.g@gmail.com>
2023-04-17 22:12:30 +0200

    task: add partial implementation of VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,82 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  isReverse;
+
+  constructor(isReverse) {
+    this.isReverse = isReverse === false;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  reverse(str) {
+    return str.split('').reverse().join('');
+  }
+
+  encrypt(str, key) {
+    if (typeof str !== 'string' || typeof key !== 'string') {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const klen = key.length;
+    const base = 'a'.charCodeAt(0);
+
+    let result = '';
+
+    for (let i = 0, k = 0, len = str.length; i < len; ++i) {
+      const char = str[i].toLowerCase();
+
+      if (!/[a-z]/i.test(char)) {
+        result += char;
+        continue;
+      }
+
+      const code = char.charCodeAt(0) - base;
+      const kcode = key[k].toLowerCase().charCodeAt(0) - base;
+
+      result += String.fromCharCode(base + (code + kcode) % 26 - 32);
+      k = (k + 1) % klen;
+    }
+
+    return this.isReverse ? this.reverse(result) : result;
+  }
+
+  decrypt(str, key) {
+    if (typeof str !== 'string' || typeof key !== 'string') {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const klen = key.length;
+    const base = 'a'.charCodeAt(0);
+
+    let result = '';
+
+    for (let i = 0, k = 0, len = str.length; i < len; ++i) {
+      const char = str[i];
+
+      if (!/[a-z]/i.test(char)) {
+        result += char;
+        continue;
+      }
+
+      const code = char.charCodeAt(0) - base + 32;
+      const kcode = key[k].charCodeAt(0) - base;
+
+      const shift = code < kcode ? 26 : 0;
+
+      result += String.fromCharCode(base + (code - kcode) % 26 + shift - 32);
+      k = (k + 1) % klen;
+    }
+
+    return this.isReverse ? this.reverse(result) : result;
   }
 }
+
+const directMachine = new VigenereCipheringMachine();
+const reverseMachine = new VigenereCipheringMachine(false);
+
+console.log(
+  // directMachine.encrypt('Example of sequence: 1, 2, 3, 4.', 'lilkey'),
+  directMachine.encrypt('Samelengthkey', 'Samelengthkey'),
+);
 
 module.exports = {
   VigenereCipheringMachine
